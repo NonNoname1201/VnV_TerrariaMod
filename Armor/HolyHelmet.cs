b4d827d74d81cbf3cfb5bc4316b04a64a241a4cf
Volodymyr Dobrohorskyi <dobrohorskyi.v@gmail.com>
2023-09-19 23:41:50 +0200

    Added Helmet, all textures are templates

---
+++ Armor/HolyHelmet.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TLoh.Armor;
+
+[AutoloadEquip(EquipType.Head)]
+public class HolyHelmet : ModItem
+{
+    public override void SetStaticDefaults() 
+    {
+        DisplayName.SetDefault("Holy Helmet");
+        Tooltip.SetDefault("Forged in god's image");
+    }
+
+    public override void SetDefaults() 
+    {
+        Item.width = 30;
+        Item.height = 30;
+        Item.value = Item.buyPrice(gold: 10);
+        Item.rare = 8;
+        Item.headSlot = 0;
+    }
+
+    public override void UpdateEquip(Player player)
+    {
+        player.endurance += 0.5f;
+        player.statDefense += 10;
+        player.lifeRegen += 5;
+        player.luck += 5;
+    }
+
+    public override void AddRecipes() 
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.SilverHelmet);
+        recipe.AddIngredient(ItemID.LunarBar, 5);
+        recipe.AddIngredient(ItemID.HallowedBar, 20);
+        recipe.AddIngredient(ItemID.GoldBar, 20);
+        recipe.AddIngredient(ItemID.SoulofLight, 10);
+            
+        recipe.AddTile(TileID.LunarCraftingStation);
+        recipe.Register();
+    }
+}
